67307b380f9d6507b6c201a296eb7cab610f7cab
erina <jerzy.dominiak.wazne@gmail.com>
2024-07-22 09:34:39 +0200

    asd

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#wykorzystuje biblioteke scrapy
+#crawler/crawler/spiders/crawling_spiders do sprawdzenia samego crawlera
 from scrapy.crawler import CrawlerProcess
 from crawler.crawler.spiders.crawling_spiders import CrawlingSpider
 
